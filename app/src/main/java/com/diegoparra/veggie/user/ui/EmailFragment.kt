93657e53de31e007a1a9d04dd7399c6d9db75e20
diegoparra <dfparra11@gmail.com>
2021-05-23 01:02:01 -0500

    -> Changes in user package/module. -> Some small changes and refactoring in products.
    ------------------------------------------
    Changes in user:
    
    - Created EmailAuthViewModel.
    - Created IsEmailRegisteredUseCase.kt
    - Added popUpTo and popUpToInclusive in navigation graphs, to improve navigation and not show user details when pressing back from SignInOptions (login screen).
    - Corrected navigations in signIn. Email should always navigate to signIn and if email has not been registered, user has not been created with that email, then signIn must send to signUp.
      When trying to use LiveData and directly send to the correct one from email, I could not pres back as LiveData will be reloaded and navigation to signIn/signUp will be reloaded.
    
    ----------------------------------------------
    
    Changes in products:
    -> Tried to move getPriceBeforeDiscount outside ui, but that value is so simple and just needed in the ui, it is not needed for anything in the domain. In addition, placing that in the domain would cause errors, as someone could operate and increase or decerase that value, without modifying discount or price, and that would result in inconsistent value shown in the ui. As the value is simple to calculate with just price and discount, and is just needed in the ui, it can be calculated and shown in the ui.
    -> Other changes:
      - Added rounding numbers logic in the core module, IntUtils.kt
      - Refactored (in Basics.kt) addThousandSeparator to addPriceFormat -> which return a price with $_,___ and rounded to multiple of 10.
    
    -> Removed Description class. It was not strictly necessary, it was just acting as a wrapper.

--- app/src/main/java/com/diegoparra/veggie/user/ui/EmailFragment.kt
+++ app/src/main/java/com/diegoparra/veggie/user/ui/EmailFragment.kt
@@ -5,15 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.diegoparra.veggie.R
+import com.diegoparra.veggie.core.Either
+import com.diegoparra.veggie.core.Failure
 import com.diegoparra.veggie.databinding.FragmentEmailBinding
+import com.diegoparra.veggie.user.viewmodels.EmailAuthViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
 @AndroidEntryPoint
 class EmailFragment : Fragment() {
 
     private var _binding : FragmentEmailBinding? = null
     private val binding get() = _binding!!
-
+    //private val viewModel: EmailAuthViewModel by hiltNavGraphViewModels(R.id.nav_sign_in)
+    private val viewModel: EmailAuthViewModel by viewModels()
+    //private val viewModel: EmailAuthViewModel by navGraphViewModels(R.id.nav_main)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +32,29 @@ class EmailFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.btnContinue.setOnClickListener {
+            val result = viewModel.setEmail(
+                binding.email.text.toString().trim()
+            )
+            when(result){
+                is Either.Right ->{
+                    binding.emailLayout.error = null
+                    //  Always navigate to sign in. In signIn check and if there is no account created
+                    //  with that email, send to create account / signUp.
+                    findNavController().navigate(EmailFragmentDirections.actionEmailFragmentToEmailSignInFragment())
+                }
+                is Either.Left -> {
+                    val message = when(result.a){
+                        is Failure.SignInFailure.EmptyField -> getString(R.string.failure_empty_field, "Email")
+                        is Failure.SignInFailure.InvalidEmail -> getString(R.string.failure_invalid_email)
+                        else -> null
+                    }
+                    binding.emailLayout.error = message
+                }
+            }
+        }
+    }
 
 
     override fun onDestroyView() {
